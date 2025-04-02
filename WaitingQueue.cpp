614c7a428cd6c3e255bcc7f78bcc1a9968ff7b9a
rodrigo-ribeir <rodrigodacruzribeiro10@gmail.com>
2025-04-02 16:10:15 -0300

    Função enqueue

--- WaitingQueue.cpp
+++ WaitingQueue.cpp
@@ -2,13 +2,44 @@
 
 namespace WaitingQueueTAD {
 
-WaitingQueue createQueue() {
+WaitingQueue* createQueue() {
     WaitingQueue* queue = new WaitingQueue;
     queue->generalCounter = 0;
     queue->elderlyCounter = 0;
-    queue->head = nullptr;
-    queue->tail = nullptr;
-    return queue;
+     // Inicializa as filas de idosos e geral
+     queue->elderlyQueue.head = nullptr;
+     queue->elderlyQueue.tail = nullptr;
+     queue->elderlyQueue.count = 0;
+ 
+     queue->generalQueue.head = nullptr;
+     queue->generalQueue.tail = nullptr;
+     queue->generalQueue.count = 0;
+ 
+     return queue;
 }
+
+void enqueue(WaitingQueue* queue, Client client) {
+    QueueNode* newNode = new QueueNode{client, nullptr};
+
+    if (client.priority == 1) { // Cliente idoso
+        if (!queue->elderlyQueue.head) { // Fila vazia
+            queue->elderlyQueue.head = queue->elderlyQueue.tail = newNode;
+        } else {
+            queue->elderlyQueue.tail->next = newNode;
+            queue->elderlyQueue.tail = newNode;
+        }
+        queue->elderlyQueue.count++;
+        queue->elderlyCounter++;
+    } else { // Cliente geral
+        if (!queue->generalQueue.head) { // Fila vazia
+            queue->generalQueue.head = queue->generalQueue.tail = newNode;
+        } else {
+            queue->generalQueue.tail->next = newNode;
+            queue->generalQueue.tail = newNode;
+        }
+        queue->generalQueue.count++;
+    }
+}
+
 
 }
--- WaitingQueue.h
+++ WaitingQueue.h
@@ -1,26 +1,36 @@
 #ifndef WAITINGQUEUE.H
 #define WAITINGQUEUE.H
 
+
+
 struct Client {
     char name[50];
     int priority; // 0 geral; 1 idoso
 };
 
+
 struct QueueNode {
     Client client;
     QueueNode* next;
     QueueNode* previous;
+};
 
-
-
+struct Queue {
+    QueueNode* head;  // Primeiro elemento da fila
+    QueueNode* tail;   // Último elemento da fila
+    int count;         // Número de elementos na fila
 };
 
 struct WaitingQueue {
-    int generalCounter;
-    int elderlyCounter;
-
+    int elderlyCounter;  // Número de idosos na fila
+    int generalCounter;  // Número de clientes gerais na fila
+    Queue elderlyQueue;  // Fila de idosos
+    Queue generalQueue;  // Fila de clientes gerais
 };
 
+
 WaitingQueue* createQueue();
+
+void enqueue(WaitingQueue* queue, Client client);
 
 #endif
